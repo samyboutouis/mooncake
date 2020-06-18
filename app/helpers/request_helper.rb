e5cf49c20a7b14edd79893b3f4a2c731dc7c8cca
Samy Boutouis <sb590@duke.edu>
2020-06-18 08:56:11 -0500

    add logic to helper method

---
+++ app/helpers/request_helper.rb
@@ -0,0 +1,11 @@
+module RequestHelper
+    def active_request(request)
+        if request.status == 'Under Review'
+            return '<div class="circle persimmon"></div>'.html_safe
+        elsif request.status == 'Denied'
+            return '<div class="circle red"></div>'.html_safe
+        else
+            return '<div class="circle green"></div>'.html_safe
+        end
+    end
+end
