239b337122b842992803d00e841ca0f1e2f36c0a
Samy Boutouis <sb590@duke.edu>
2020-06-18 16:33:28 -0500

    Some non-functioning javascript code

--- app/helpers/request_helper.rb
+++ app/helpers/request_helper.rb
@@ -1,7 +1,7 @@
 module RequestHelper
     def active_request(request)
         if request.status == 'Under Review'
-            return '<div class="circle persimmon"></div>'.html_safe
+            return '<div id="circle" class="circle persimmon"></div>'.html_safe
         elsif request.status == 'Denied'
             return '<div class="circle red"></div>'.html_safe
         else
