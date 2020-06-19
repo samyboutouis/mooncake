0701d1364403b0094f7bb9452d129e50c3477258
ms858 <ms858@duke.edu>
2020-06-19 14:10:33 -0400

    Update ldap.rb

--- ldap.rb
+++ ldap.rb
@@ -1,77 +1,46 @@
 require 'rubygems'
 require 'net/ldap'
 
-class LdapAuthorize 
+class LdapAuthorize
 end
     def authorize
         ldap = Net::LDAP.new
         ldap.host = "ldap.duke.edu"
         base = "dc=duke,dc=edu"
         ldap.port = 389
-        auth = {:method => :simple, :username => "ms858", :password => "66Mary"}
-        auth = auth
         if ldap.bind
             puts "authentication succeeded"
         else
             puts "authentication failed"
         end
-        # puts "enter netid: "
-        # netid = gets.chomp()
-        # puts "#{netid}"
-        # user = User.find_or_create_by(email: email)
-        ldap = Net::LDAP.new :host => "ldap.duke.edu", :port => 389
-        filter = Net::LDAP::Filter.eq( "Email", "maryam.shahid@duke.edu" )
-        
-        ldap.search( :base => base, :filter => filter ) do |entry|
+        puts "enter netid: "
+        netid = gets.chomp()
+        filter = Net::LDAP::Filter.eq( "cn", netid )
+
+        result = ldap.search( :base => base, :filter => filter ) do |entry|
             puts "DN: #{entry.dn}"
             entry.each do |attribute, values|
-              puts "   #{attribute}:"
               values.each do |value|
-                puts "      --->#{value}"
+                puts " #{attribute} --->#{value}"
               end
             end
         end
-        p ldap.get_operation_result
-            
+        ldap.get_operation_result
+
+        puts "****************************"
+        puts "*****Authorizaing.......****"
+        puts "****************************"
+        
+        information = result.pop
+        affiliation = information["edupersonprimaryaffiliation"]
+
+
+        if affiliation[0] == "staff"
+            puts "You are authorized"
+        else
+            puts "Not Authorized!"
+        end
+
     end
 
     authorize
-
-
-
-
-
-# ldap = Net::LDAP.new
-# ldap.host = ldap.duke.edu
-# ldap.port = 389
-# ldap.auth "joe_user", "opensesame"
-# if ldap.bind
-#   # authentication succeeded
-# else
-#   # authentication failed
-# end
-
-# puts "enter netid: "
-# netid = gets.chomp()
-# ldap = Net::LDAP.new :host => server_ip_address,
-#      :port => 389,
-#      :auth => {
-#            :method => :simple,
-#            :username => "dc=duke,dc=edu",
-#            :password => "opensesame"
-#      }
-
-# filter = Net::LDAP::Filter.eq( "cn", "George*" )
-# treebase = "dc=example,dc=com"
-
-# ldap.search( :base => treebase, :filter => filter ) do |entry|
-#   puts "DN: #{entry.dn}"
-#   entry.each do |attribute, values|
-#     puts "   #{attribute}:"
-#     values.each do |value|
-#       puts "      --->#{value}"
-#     end
-#   end
-# end
-
-# p ldap.get_operation_result
