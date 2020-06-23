60dba904454354cd39976b24289e8beae3d07196
vkp3 <vkp3@duke.edu>
2020-06-23 11:55:54 -0400

    fixed failed tests

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,7 @@
 class Course < ApplicationRecord
+    has_and_belongs_to_many :users
+    has_and_belongs_to_many :prereqs
+    has_and_belongs_to_many :questions
+    has_many :course_requests
+    has_many :permission_numbers
 end
