6bb5ffa682b5cedceb2753e6c19ccd0e18b1da83
ms858 <ms858@duke.edu>
2020-06-23 10:06:06 -0400

    working on form

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -1,9 +1,11 @@
 class FormController < ApplicationController
     def form 
+        @user = $current_user
     end
 
     def data
         # retrieve data using param
+        @user = $current_user
         puts params["major"]
         redirect_to root_url
     end
