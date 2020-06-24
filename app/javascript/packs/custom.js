dbd7207a4c64ef235551a4f3cc5b09b40b62046f
Samy Boutouis <sb590@duke.edu>
2020-06-24 08:34:38 -0500

    Readability improvements

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -20,26 +20,30 @@ $(document).ready(function () {
     });
   });
 
-  $(".department").change(function() {
-    let selected = $(this).val();
-    $(".section").empty();
-    $.ajax('/department', {
-      type: 'GET',
-      dataType: 'json',
-      data: {department: selected},
-      success: function(result) {
-        console.log('Success');
-        $(".section").append("<option>Choose Course Number</option>");
-        for (var i = 0; i < result.length; i++) {
-          $(".section").append("<option>" + result[i].course_number + "</option>");
-        }
-      },
-      error: function() {
-        console.log('Error');
+  $(".department").on('change', function() {
+    getSection($(this));
+  });
+});
+
+function getSection(element) {
+  let selected = element.val();
+  $(".section").empty();
+  $.ajax('/department', {
+    type: 'GET',
+    dataType: 'json',
+    data: {department: selected},
+    success: function(result) {
+      console.log('Success');
+      $(".section").append("<option>Choose Course Number</option>");
+      for (var i = 0; i < result.length; i++) {
+        $(".section").append("<option>" + result[i].course_number + "</option>");
       }
-    });
-  }) 
-})
+    },
+    error: function() {
+      console.log('Error');
+    }
+  });
+}
 /*
 
 $(document).ready(function(){
