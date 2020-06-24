3dad9a64c009371cd7996611fc2735a8c5c2698f
ms858 <ms858@duke.edu>
2020-06-24 17:47:57 -0400

    courses/questions complete with default

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,14 +3,19 @@ class QuestionsController < ApplicationController
         params.require(:question).permit(:question_type, :question_text)
     end
 
+    # initializing the course
     def course
         $course = nil
         $course = Course.find(params[:course])
         redirect_to question_path
     end
 
+    # main page for creating form
     def create_form
         $course.questions = Course.find($course.id).questions
+        if $course.questions.count == 0
+            $course.questions << Question.find(42,43,44)
+        end
         @question = $course.questions
     end
 
@@ -19,10 +24,12 @@ class QuestionsController < ApplicationController
         redirect_to question_path
     end
 
+    # add new question
     def new
         @question = Question.new
     end
 
+    # delete question
     def delete
         $course.questions.destroy(Question.find(params[:id]))
         Question.destroy(Question.find(params[:id]))
