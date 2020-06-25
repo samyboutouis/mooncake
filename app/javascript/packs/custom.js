be09d9bc6e7c8aa45e2ce3352d821e64fe85ddbd
vkp3 <vkp3@duke.edu>
2020-06-25 17:00:58 -0400

    can assign multiple prereqs

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,3 +1,4 @@
+var count = 0;
 $(document).ready(function () {
   $(".request-box").on('click', function() {
     console.log('clicked');
@@ -28,8 +29,8 @@ $(document).ready(function () {
     getSection($(this));
   });
 
-  $("#prereq").on('click', function() {
-    createField($(this));
+  $(document).on('click', "#add-prereq", function() {
+    createField();
   });
 
 });
@@ -76,18 +77,12 @@ function getSection(element) {
 }
 
 
-function createField(element) {
-
-  $.ajax('/courses/new', {
-    success: function(result) {
-      console.log('Success');
-      // $(".addNew").append(".prereq_field");
-      $('.prereqfield:first').clone().appendTo(".addNew");
-    },
-    error: function() {
-      console.log('Error');
-    }
-  });
+function createField() {
+    count++;
+    var clonedField = $(".prereqfield:first").clone();
+    console.log(clonedField);
+    clonedField.attr("name", "prereq_attributes["+ count + "]name" );
+    clonedField.appendTo(".addNew");
 }
 
 
