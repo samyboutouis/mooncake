afcb73c82bbd951698319134f9bedf8bbfcaa4b5
Sunny Li <sunnyli@Sunnys-MacBook-Air.local>
2020-06-26 11:08:12 -0500

    displays faculty courses

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,13 @@
 module CoursesHelper
+  def my_courses(course)
+    return nil
+  end
+
+  def no_course(user)
+      if user.courses.length() == 0
+          return '<p>You have created no courses for this term</p>'.html_safe
+      else
+          return nil
+      end
+  end
 end
