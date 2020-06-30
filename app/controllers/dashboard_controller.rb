98fe599be8a7c073ff2d673067d97e2ed517bd84
ms858 <ms858@duke.edu>
2020-06-30 10:34:53 -0400

    Cleaned extra files + added stuff to html

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -27,49 +27,6 @@ class DashboardController < ApplicationController
 
   def facreqview
     @course = Course.first
-    # @course_request = @course.course_requests
-    # if params[:question_text].present?
-    #   @question = Question.find_by(question_text: params[:question_text])
-    #   @answer = @question.answers.order(:answer_text)
-    # else
-    #   @answer = Question.first.answers
-    # end
   end
-
-
-  #FAILED ATTEMPTS
-
-    # list = []
-    # for request in @course_request do
-    #   h = {}
-    #   for question in request.questions do
-    #     ans = request.answers.find_by(question_id: question.id).answer_text
-    #     h[question.question_text] = ans
-    #   end
-    #   list.append(h)
-    # end
-
-    # @list = list
-    # sort = params[:sort]
-    # sort = nil unless sort.in?(['distance', 'name', 'price', 'price desc', 'review'])
-    # @course_request = CourseRequest.oAnswer.order(sort)
-    # @question = @course.questions
-    # if params.include?("question_text")
-    #   @question = @course.questions.find_by(question_text: params[:question_text])
-    # end
-
-
-#@question = @course.questions.find_by(question_text: "Major:")
-  
-  
-
-  # private
-  # def sort_column
-  #   CourseRequest.first.questions.pluck(:question_text).include?(params[:sort]) ? params[:sort] : "Grad Year"
-  # end
-  
-  # def sort_direction
-  #   %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
-  # end
 
 end
