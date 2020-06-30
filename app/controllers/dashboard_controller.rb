9b1ab78df69184cb48f1f409b7daee9b87dad3e2
ms858 <ms858@duke.edu>
2020-06-30 00:57:40 -0400

    sorted model, need to display changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -28,12 +28,17 @@ class DashboardController < ApplicationController
   def facreqview
     @course = Course.first
     @course_request = @course.course_requests
-    @answer = @course_request.answers.order(:answer_text)
+    if params[:question_text].present?
+      @question = Question.find_by(question_text: params[:question_text])
+      @answer = @question.answers.order(:answer_text)
+    else
+      @answer = Question.first.answers
+    end
+  end
 
 
-
-
-
+  #FAILED ATTEMPTS
+    
     # list = []
     # for request in @course_request do
     #   h = {}
@@ -52,13 +57,11 @@ class DashboardController < ApplicationController
     # if params.include?("question_text")
     #   @question = @course.questions.find_by(question_text: params[:question_text])
     # end
-  end
 
 
 #@question = @course.questions.find_by(question_text: "Major:")
   
-  def sort  
-  end
+  
 
   # private
   # def sort_column
--- app/javascript/packs/facreqview.js
+++ app/javascript/packs/facreqview.js
@@ -1,23 +1,32 @@
 $(document).ready(function () {
   // var data = JSON.parse(JSON.parse(@json).data);
 
-
-
   $(".question").on('change', function() {
-    getNumber($(this));
+    sort($(this));
   });
 
 });
 
 
-function getNumber(element) {
+function sort(element) {
   let selected = element.val();
+  $('#answers').empty();
+  console.log(selected);
   $.ajax('/facreqview', {
     type: 'GET',
-    dataType: 'json',
     data: {question_text: selected},
     success: function(result) {
+      // $('#answers').load(" #answers>*");
+      // location.reload();
       console.log('Success');
+      // $('#answers').append(" <% @answer.each do |answer| %>").html_safe;
+      // $('#answers').append(" <tr>");
+      // $('#answers').append(" <% answer.course_request.answers.each do |answer| %>").htmlerb_safe;
+      // $('#answers').append("<td><%= answer.answer_text %></td>").html_safe;
+      // $('#answers').append("<% end %>").html_safe;
+      // $('#answers').append(" <% @answer.each do |answer| %>").html_safe;
+      // $('#answers').append(" </tr>");
+      // $('#answers').append(" <% end %>").html_safe;
     },
     error: function() {
       console.log('Error');
