df4536f9190c595cd35b254ef1028d06813dcf14
Samy Boutouis <sb590@duke.edu>
2020-06-30 15:17:29 -0500

    Fixing course deletion bug

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,7 +8,15 @@ class Course < ApplicationRecord
     before_destroy do
         questions.each { |question| question.destroy if question.id>6 }
     end
-    has_many :course_requests, :dependent => :delete_all
+    has_many :course_requests
+    before_destroy do
+        course_requests.each do |course_request|
+            course_request.answers.each do |answer|
+                answer.destroy
+            end
+        course_request.destroy
+        end
+    end
     has_many :permission_numbers, :dependent => :delete_all
     accepts_nested_attributes_for :prereqs, allow_destroy: true
 end
