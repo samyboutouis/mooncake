4a022c70e8721a4310053cb21d74fd11febcdfe5
Samy Boutouis <sb590@duke.edu>
2020-07-01 13:26:40 -0500

    Confirmation for buttons

--- app/javascript/packs/facreqview.js
+++ app/javascript/packs/facreqview.js
@@ -4,16 +4,6 @@ $(document).ready(function () {
         order: [[ 1, 'asc' ]],
     } );
 
-    $(".accept").on('click', function() {
-        var data = table.row( $(this).parents('tr') ).data();
-        confirm("Are you sure you want to accept "+ data[0] + "'s request?");
-    });
-
-    $(".deny").on('click', function() {
-        var data = table.row( $(this).parents('tr') ).data();
-        confirm("Are you sure you want to deny "+ data[0] + "'s request?");
-    });
-
     $('#myTable tbody').on('click', 'td.details-control', function () {
         var tr = $(this).closest('tr');
         var row = table.row( tr );
@@ -31,15 +21,13 @@ $(document).ready(function () {
     } );
 });
 
-
-
 function format ( d ) {
     // `d` is the original data object for the row
     
     return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
         '<tr>'+
             '<td>Full name:</td>'+
-            '<td>'+d[0]+'</td>'
+            '<td>'+d[0]+'</td>'+
         '</tr>'+
     '</table>';
 }
