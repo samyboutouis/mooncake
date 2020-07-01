8098e2fd33d09b4a55b6f8fb10cb0db83e77e522
ms858 <ms858@duke.edu>
2020-07-01 14:33:32 -0400

    Auto mailer on course creation with requests page link

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,14 @@ class UserMailer < ApplicationMailer
  
   def welcome_email
     @email = params[:email]
+    @url = "http://localhost:3000"
     mail(to: @email, subject: 'Welcome to My Awesome Site')
+  end
+
+  def course_created
+      @user = params[:user]
+      @course = params[:course]
+      @url = "http://localhost:3000/facreqview/" + @course.id.to_s
+      mail(to: @user.email, subject: 'New Course Created')
   end
 end
