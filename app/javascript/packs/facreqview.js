af978720ed5b7c03fe3ff5f20e452a8014819d54
Samy Boutouis <sb590@duke.edu>
2020-07-01 15:56:05 -0500

    Expandable row

--- app/javascript/packs/facreqview.js
+++ app/javascript/packs/facreqview.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
     var table = $('#myTable').DataTable( {
-        columnDefs: [ { orderable: false, targets: [0] } ],
+        columnDefs: [ 
+            { 
+                orderable: false, 
+                targets: [0] 
+            }],
         order: [[ 1, 'asc' ]],
     } );
+    let num = table.columns().count()
+    for (let i = 7; i < num - 1; i++){
+        table.column(i).visible(false);
+    }
 
     $('#myTable tbody').on('click', 'td.details-control', function () {
         var tr = $(this).closest('tr');
@@ -15,19 +23,18 @@ $(document).ready(function () {
         }
         else {
             // Open this row
-            row.child( format(row.data()) ).show();
+            row.child( format(row.data(), table) ).show();
             tr.addClass('shown');
         }
     } );
 });
 
-function format ( d ) {
+function format ( d, table ) {
     // `d` is the original data object for the row
-    
-    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
-        '<tr>'+
-            '<td>Full name:</td>'+
-            '<td>'+d[0]+'</td>'+
-        '</tr>'+
-    '</table>';
+    let innards = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
+    for(let i = 7; i < table.columns().count() - 1; i++){
+        innards += '<tr>'+ '<td>'+$(table.column(i).header()).html()+'</td>'+ '<td>'+d[i]+'</td>'+'</tr>';
+    }
+    innards += '</table>';
+    return innards;
 }
