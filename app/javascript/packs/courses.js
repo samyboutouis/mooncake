f71747fcee463ba53516969142f18d2399e69392
vkp3 <vkp3@duke.edu>
2020-07-07 14:44:27 -0400

    file upload styling change

--- app/javascript/packs/courses.js
+++ app/javascript/packs/courses.js
@@ -4,6 +4,11 @@ $(document).ready(function () {
   $("#add-prereq").on('click', function() {
     createField();
   });
+  $(".custom-file-input").on("change", function() {
+    var fileName = $(this).val().split("\\").pop();
+    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+  });
+  
 });
 
 
