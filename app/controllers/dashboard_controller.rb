9b1940eb5803027560e2bf86a15da0783cd2027e
ms858 <ms858@duke.edu>
2020-07-07 09:58:00 -0400

    uncomment mailer

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -29,7 +29,7 @@ class DashboardController < ApplicationController
     req = CourseRequest.find(params[:request])
     course = CourseRequest.find(params[:request]).course
     CourseRequest.find(params[:request]).update(status: "Denied")
-    # UserMailer.with(user: session[:current_user], request: req).status_changed.deliver_now
+    UserMailer.with(user: session[:current_user], request: req).status_changed.deliver_now
     redirect_to requests_page_path(course)
   end
 
@@ -42,12 +42,12 @@ class DashboardController < ApplicationController
       req.update(status: "Accepted")
       course.increment!(:seats_taken)
       if course.seats_taken >= course.capacity
-        # UserMailer.with(user: session[:current_user], course: course).capacity_reached.deliver_now
+        UserMailer.with(user: session[:current_user], course: course).capacity_reached.deliver_now
       end
       perm = course.permission_numbers.where(used: false).last
       perm.course_request = req
       perm.update(used: true)
-      # UserMailer.with(user: session[:current_user], request: req).status_changed.deliver_now
+       UserMailer.with(user: session[:current_user], request: req).status_changed.deliver_now
       redirect_to requests_page_path(course)
     end
     
