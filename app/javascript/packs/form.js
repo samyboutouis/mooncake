ff25a41c5de80dab4dfdb6fa0ac67ccb5200a9a0
Samy Boutouis <sb590@duke.edu>
2020-07-08 08:55:01 -0500

    styling changes to crosslisting form

--- app/javascript/packs/form.js
+++ app/javascript/packs/form.js
@@ -12,7 +12,7 @@ function getResponse(element) {
   $("#howmany").empty();
   if (selected === "Yes") {
     $("#howmany").append("<label for= 'number-choice'>How many courses are cross-listed?</label>");
-    $("#howmany").append('<input type="text" name= "number-choice" id="number-choice" maxlength= 2><br>');
+    $("#howmany").append('<input type="text" name= "number-choice" id="number-choice" class="form-control" maxlength= 2><br>');
   }
   else {
     return;
@@ -25,15 +25,16 @@ function getOptions(element) {
   for(let i = 0; i < selected; i++) {
     $("#othercourses").append("<h3> Cross-listed Course " + (i+1) + "</h3>");
     $("#othercourses").append("<label for= 'department"+(i+1)+"'> Deparment:</label>");
-    $("#othercourses").append('<input name= "department'+(i+1)+'" type="text" id="department'+(i+1)+'"><br>');
+    $("#othercourses").append('<input name= "department'+(i+1)+'" type="text" class="form-control" id="department'+(i+1)+'"><br>');
 
     $("#othercourses").append("<label for= 'course_number"+(i+1)+"'> Course Number:</label>");
-    $("#othercourses").append('<input name= "course_number'+(i+1)+'" type="text" id="course_number'+(i+1)+'"><br>');
+    $("#othercourses").append('<input name= "course_number'+(i+1)+'" type="text" class="form-control" id="course_number'+(i+1)+'"><br>');
 
     $("#othercourses").append("<label for= 'section_number"+(i+1)+"'> Section Number:</label>");
-    $("#othercourses").append('<input name= "section_number'+(i+1)+'" type="text" id="section_number'+(i+1)+'"><br>');
+    $("#othercourses").append('<input name= "section_number'+(i+1)+'" class="form-control" type="text" id="section_number'+(i+1)+'"><br>');
 
-    $("#othercourses").append("<label for= 'file"+(i+1)+"'> Upload Permission Numbers CSV File</label>");
+    $("#othercourses").append("<label for= 'file"+(i+1)+"'> Upload Permission Numbers CSV File</label><br>");
     $("#othercourses").append('<input name= "file'+(i+1)+'" type="file" id="file'+(i+1)+'"><br>');
+    $("#othercourses").append('<div class="spacer"></div>');
   }
 }
