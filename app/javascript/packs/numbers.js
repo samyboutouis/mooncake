c93e47b489fa3b8a690dd2388b818c9f04dd2523
Elizabeth Zhang <eyz3@duke.edu>
2020-07-14 11:44:32 -0400

    numbered table

--- app/javascript/packs/numbers.js
+++ app/javascript/packs/numbers.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
     var table = $('#numTable').DataTable( {
+      columnDefs: [ 
+        { 
+            orderable: false, 
+            targets: [0] 
+        }],
       order: [[ 1, 'asc' ]],
     } );
+  });
