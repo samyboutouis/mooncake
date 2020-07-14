f345f0dbb6d573a7baad6fabaaa90a85cdfcaf13
taiyangie <sunnyxli2019@gmail.com>
2020-07-14 10:44:00 -0500

    datables pagination

--- app/javascript/packs/numbers.js
+++ app/javascript/packs/numbers.js
@@ -1,3 +1,4 @@
 $(document).ready( function () {
-    $('#numTable').DataTable();
-} );
+    $('#numTable').DataTable({"aLengthMenu": [[10, 20, 50, 100, -1], [10, 20, 50, 100, "All"]]});
+
+    } );
