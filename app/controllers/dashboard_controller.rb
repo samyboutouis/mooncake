862241bdd6816bfa75ddb3dbc67fd11440208296
Elizabeth Zhang <eyz3@duke.edu>
2020-07-15 16:58:56 -0400

    publish form and add more perm num buttons

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -90,12 +90,64 @@ class DashboardController < ApplicationController
     course = CourseRequest.find(params[:request]).course
     file = params[:file]
     @request = CourseRequest.find(params[:request])
-    CSV.foreach(file, :headers => true) do |row|
-      unless row[0] == nil
-        course.permission_numbers.create(number: row[0], expire_date: row[7], used: false)
+    xlsx = Roo::Spreadsheet.open(file)
+    sheet = xlsx.sheet(0)
+    z=0
+    sheet.each do |row|
+      if z>0
+        unless row[0] == nil
+          consent = false
+          reqs = false
+          capacity = false
+          if row[8] == "Y"
+            capacity = true
+          end
+          if row[9] == "Y"
+            reqs = true
+          end
+          if row[10] == "Y"
+            consent = true
+          end
+          course.permission_numbers.create(number: row[0].to_i, expire_date: row[7], used: false, consent: consent, capacity: capacity, reqs: reqs)
+        end
       end
+      z += 1
     end
     redirect_to accept_path(@request)
+  end
+
+  def addmorepermnum
+    @course = Course.find(params[:course])
+  end
+
+  def addmore
+    course = Course.find(params[:course]) #CourseRequest.find(params[:request]).course
+    file = params[:file]
+    #@request = CourseRequest.find(params[:request])
+    xlsx = Roo::Spreadsheet.open(file)
+      sheet = xlsx.sheet(0)
+      z=0
+      sheet.each do |row|
+        if z>0
+          unless row[0] == nil
+            consent = false
+            reqs = false
+            capacity = false
+            if row[8] == "Y"
+              capacity = true
+            end
+            if row[9] == "Y"
+              reqs = true
+            end
+            if row[10] == "Y"
+              consent = true
+            end
+            course.permission_numbers.create(number: row[0].to_i, expire_date: row[7], used: false, consent: consent, capacity: capacity, reqs: reqs)
+          end
+        end
+        z += 1
+      end  
+    redirect_to numbers_path(course)
   end
 
   def rank1
