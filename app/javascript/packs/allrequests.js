02e0e6f6791acd4388326acfbe9cd1750ace2a3a
Elizabeth Zhang <eyz3@duke.edu>
2020-07-16 10:53:48 -0400

    email selected button

--- app/javascript/packs/allrequests.js
+++ app/javascript/packs/allrequests.js
@@ -1,4 +1,8 @@
 $(document).ready( function () {
     $('#allrequestsTable').DataTable({"aLengthMenu": [[10, 20, 50, 100, -1], [10, 20, 50, 100, "All"]]});
+
+    $("#email-all").on('click', function() {
+        $("#hidden-email-all").click();
+      });
       
 });
