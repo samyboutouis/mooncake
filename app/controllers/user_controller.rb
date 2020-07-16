89a3357f48d70eaf6e32f462e016a5ce4258acc9
ms858 <ms858@duke.edu>
2020-07-16 09:36:23 -0400

    minor changes and reseeding

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,10 +4,5 @@ class UserController < ApplicationController
   
   def show
       @user = User.find_by(net_id: session[:current_user]["net_id"])
-
-      if User.exists?
-        # Tell the UserMailer to send a welcome email after save
-        UserMailer.with(email: @user.email).welcome_email.deliver_now
-      end
   end
 end
