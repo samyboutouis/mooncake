f118bed7c4d2d79d214dfc0167959d09164f03cc
ms858 <ms858@duke.edu>
2020-07-23 12:27:37 -0400

    faculty recieves email when requests are 10,20, 30...

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -10,6 +10,29 @@ class AnswerController < ApplicationController
         @user = User.find_by(net_id: session[:current_user]["net_id"])
         @course = Course.find_by(department: params[:department], course_number: params[:course_number], section_number: params[:section_number])
         @course_request = CourseRequest.create(status: "Under Review", course: @course, user: @user)
+        if @course.primary == false
+            course1 = @course.cross_listing[0]
+            total = course1.course_requests.count
+            name = course1.department.split("-").first + " "+ course1.course_number + " - " + course1.section_number
+            for id in course1.cross_listing 
+                c = Course.find(id)
+                total += c.course_requests.count
+                name += " / " + c.department.split("-").first + " "+ c.course_number + " - " + c.section_number
+            end
+        else
+            course1 = @course
+            total = course1.course_requests.count
+            name = course1.department.split("-").first + " "+ course1.course_number + " - " + course1.section_number
+            for id in course1.cross_listing 
+                c = Course.find(id)
+                total += c.course_requests.count
+                name += " / " + c.department.split("-").first + " "+ c.course_number + " - " + c.section_number
+            end
+        end
+        if total%10 == 0
+            email = course1.user.email
+            UserMailer.with(email: email, coursename: name, total: total, course: course1.id).many_requests.deliver_now
+        end
         for question in @course_request.course.questions
             if question.question_type == "Checkbox"
                 if params[question.question_text] == nil
