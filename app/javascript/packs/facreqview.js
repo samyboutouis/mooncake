c3fa06a6f5cecd1d1e0c2d27563e3050d8dfd26c
Elizabeth Zhang <eyz3@duke.edu>
2020-07-24 12:28:39 -0400

    fa

--- app/javascript/packs/facreqview.js
+++ app/javascript/packs/facreqview.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 $(document).ready(function () {
     // document.getElementById("selected_action").style.visibility = "hidden";
 
-    var table = $('#myTable').DataTable( {
+    var table = $('#myTable').DataTable({"aLengthMenu": [[2, 20, 50, 100, -1], [2, 20, 50, 100, "All"]],
         columnDefs: [
             {
                 orderable: false,
